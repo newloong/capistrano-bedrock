c46ec3ed393288ac1751bbc3efc1701ede3adcc8
newloong <support@newloong.com>
2018-07-19 10:51:25 +0800

    Remove the space

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 # =====================
 
 # The path to local directory temporary directory which is read and writeable.
- set :local_tmp_dir, "tmp"
+set :local_tmp_dir, "tmp"
 
 # Set the name of the bedrock project folder
 set :bedrock_folder, "example.com"
