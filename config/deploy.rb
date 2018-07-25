ed4e3149727e5be2a0ce96f9069f9ee61188c31d
newloong <support@newloong.com>
2018-07-25 17:09:23 +0800

    Update deploy.rb to fix "... Permission denied"
    The error is: mkdir stderr: mkdir: cannot create directory ‘/home/welychem’: Permission denied
    
    move these line to config/deploy/development.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,19 +34,6 @@ set :wpcli_local_url, -> {"//#{fetch(:development_domain)}"}
 # Absolute or relative path to local directory for storing database backups which is read and writeable
 set :wpcli_local_db_backup_dir, "_backup_database"
 
-# Define dev role to connect to the local vagrant server
-# [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-server "#{fetch(:development_domain)}", user: 'vagrant', roles: %w{dev},
-
-ssh_options: {
-  keys: ["../#{fetch(:trellis_folder)}/.vagrant/machines/default/virtualbox/private_key"],
-  forward_agent: true,
-  auth_methods: %w(publickey)
-}
-
-# Set the local vagrant development path where the project lives
-set :dev_path, -> {"/srv/www/#{fetch(:bedrock_folder)}/current"}
-
 # Absolute path to remote project folder to deploy
 set :deploy_to, -> {"/srv/www/#{fetch(:bedrock_folder)}"}
 
